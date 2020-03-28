492f5b5b34153578131758829ca97af5ad7ad6f4
Cleyson Diego <cleyson7@hotmail.com>
2020-03-28 19:59:44 -0300

    Corrigindo lint no frontend.

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { FiLogIn } from 'react-icons/fi';
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { FiLogIn } from "react-icons/fi";
 
-import api from '../../services/api';
+import api from "../../services/api";
 
-import './styles.css';
+import "./styles.css";
 
-import logoImg from '../../assets/logo.svg';
-import heroesImg from '../../assets/heroes.png';
+import logoImg from "../../assets/logo.svg";
+import heroesImg from "../../assets/heroes.png";
 
 export default function Logon() {
-  const [id, setId] = useState('');
+  const [id, setId] = useState("");
 
   const history = useHistory();
 
@@ -18,21 +18,21 @@ export default function Logon() {
     e.preventDefault();
 
     try {
-      const response = await api.post('sessions', { id });
+      const response = await api.post("sessions", { id });
 
-      localStorage.setItem('ongId', id);
-      localStorage.setItem('ongName', response.data.name);
+      localStorage.setItem("ongId", id);
+      localStorage.setItem("ongName", response.data.name);
 
-      history.push('/profile');
+      history.push("/profile");
     } catch (err) {
-      alert('Falha no login, tente novamente.')
+      alert("Falha no login, tente novamente.");
     }
   }
 
   return (
     <div className="logon-container">
       <section className="form">
-        <img src={logoImg} alt="Be The Hero"/>
+        <img src={logoImg} alt="Be The Hero" />
 
         <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
@@ -42,7 +42,9 @@ export default function Logon() {
             value={id}
             onChange={e => setId(e.target.value)}
           />
-          <button className="button" type="submit">Entrar</button>
+          <button className="button" type="submit">
+            Entrar
+          </button>
 
           <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#e02041" />
@@ -51,7 +53,7 @@ export default function Logon() {
         </form>
       </section>
 
-      <img src={heroesImg} alt="Heroes"/>
+      <img src={heroesImg} alt="Heroes" />
     </div>
   );
 }
--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
-import { FiArrowLeft } from 'react-icons/fi';
+import { FiArrowLeft } from "react-icons/fi";
 
-import api from '../../services/api';
-import './styles.css';
+import api from "../../services/api";
+import "./styles.css";
 
-import logoImg from '../../assets/logo.svg';
+import logoImg from "../../assets/logo.svg";
 
 export default function Register() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [whatsapp, setWhatsapp] = useState('');
-  const [city, setCity] = useState('');
-  const [uf, setUf] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [whatsapp, setWhatsapp] = useState("");
+  const [city, setCity] = useState("");
+  const [uf, setUf] = useState("");
 
   const history = useHistory();
 
@@ -29,13 +29,13 @@ export default function Register() {
     };
 
     try {
-      const response = await api.post('ongs', data);
+      const response = await api.post("ongs", data);
 
       alert(`Seu ID de acesso: ${response.data.id}`);
 
-      history.push('/');
+      history.push("/");
     } catch (err) {
-      alert('Erro no cadastro, tente novamente.');
+      alert("Erro no cadastro, tente novamente.");
     }
   }
 
@@ -43,11 +43,14 @@ export default function Register() {
     <div className="register-container">
       <div className="content">
         <section>
-          <img src={logoImg} alt="Be The Hero"/>
+          <img src={logoImg} alt="Be The Hero" />
 
           <h1>Cadastro</h1>
-          <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
-          
+          <p>
+            Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem
+            os casos da sua ONG.
+          </p>
+
           <Link className="back-link" to="/">
             <FiArrowLeft size={16} color="#e02041" />
             Não tenho cadastro
@@ -55,7 +58,7 @@ export default function Register() {
         </section>
 
         <form onSubmit={handleRegister}>
-          <input 
+          <input
             placeholder="Nome da ONG"
             value={name}
             onChange={e => setName(e.target.value)}
@@ -65,17 +68,16 @@ export default function Register() {
             type="email"
             placeholder="E-mail"
             value={email}
-            onChange={e => setEmail(e.target.value)}            
+            onChange={e => setEmail(e.target.value)}
           />
 
           <input
             placeholder="WhatsApp"
             value={whatsapp}
-            onChange={e => setWhatsapp(e.target.value)}            
+            onChange={e => setWhatsapp(e.target.value)}
           />
 
           <div className="input-group">
-
             <input
               placeholder="Cidade"
               value={city}
@@ -90,8 +92,9 @@ export default function Register() {
             />
           </div>
 
-          <button className="button" type="submit">Cadastrar</button>
-
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
         </form>
       </div>
     </div>
