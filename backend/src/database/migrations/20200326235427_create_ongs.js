54d3270faec230707e6ecc6a5c70eaf81de073a1
Cleyson Diego <cleyson7@hotmail.com>
2020-03-29 16:28:37 -0300

    Funcionalidades avançadas

--- backend/src/database/migrations/20200326235427_create_ongs.js
+++ backend/src/database/migrations/20200326235427_create_ongs.js
@@ -10,5 +10,5 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTalbe("ongs");
+  return knex.schema.dropTable("ongs");
 };
--- backend/src/database/migrations/20200326235920_create_incidents.js
+++ backend/src/database/migrations/20200326235920_create_incidents.js
@@ -16,5 +16,5 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTalbe("ongs");
+  return knex.schema.dropTable("incidents");
 };
